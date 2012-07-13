ec4a4f2617c49c10d25569166c1a4cd8f011a7d0
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-13 19:30:16 +0700

    Initial commit

---
+++ lib/Task/Data/Unixish.pm
@@ -0,0 +1,18 @@
+package Task::Data::Unixish;
+
+# VERSION
+
+1;
+# ABSTRACT: Install all Data::Unixish modules
+
+=head1 DESCRIPTION
+
+This task will install all Data::Unixish modules.
+
+=pkgroup Included modules
+
+=pkg Data::Unixish
+
+=pkg Data::Unixish::Implementation
+
+=cut
